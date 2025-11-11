edc2f7a9f3426722a42083397160c193089340c8
Pawel Lisewski <dev@palekiwi.com>
2025-11-11 20:46:16 +0800

    Remove extra cat

--- lib/opencode-wrapper.nix
+++ lib/opencode-wrapper.nix
@@ -79,7 +79,7 @@ pkgs.writeShellApplication {
       -e ZAI_CODING_PLAN_API_KEY="$(cat /run/secrets/zai_coding_plan/api_key)" \
       -e OPENCODE_API_KEY="$(cat /run/secrets/opencode/api_key)" \
       -e CONTEXT7_API_KEY="$(cat /run/secrets/context7/api_key)" \
-      -e GEMINI_API_KEY="$(cat cat /run/secrets/ygt/gemini/api_key)" \
+      -e GEMINI_API_KEY="$(cat /run/secrets/ygt/gemini/api_key)" \
       -e TMPDIR="/workspace/tmp" \
       -v "opencode-cache-$PORT:/home/agent/.cache:rw" \
       -v "opencode-local-$PORT:/home/agent/.local:rw" \
